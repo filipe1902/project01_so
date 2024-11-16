0308131a506b20e98fac5d91cb06c2f861fea216
mgLogicc <filipemoitosomarques@ua.pt>
2024-11-16 19:30:19 +0000

    permissions

--- permissions.sh
+++ permissions.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Função para verificar permissões
 verificar_permissoes() {
     permissao_certa=true
 
-    if [ ! -r "$ORIGEM" ]; then #Verifica se a diretório de origem ($ORIGEM) não tem permissão de leitura (-r). Se não tiver, exibe uma mensagem de erro e define permissao_certa como false
+    if [ ! -r "$ORIGEM" ]; then 
         echo "Read permission denied for the source directory: '$ORIGEM'."
         permissao_certa=false
     fi
 
-    if [ ! -w "$BACKUP" ]; then #Verifica se o diretório de backup ($BACKUP) não tem permissão de escrita (-w). Se não tiver, exibe uma mensagem de erro e define permissao_certa como false.
+    if [ ! -w "$BACKUP" ]; then 
         echo "Write permission denied for the backup directory: '$BACKUP'."
         permissao_certa=false
     fi
 
-    #Se permissao_certa for true, a função retorna 0 (sucesso). Caso contrário, retorna 1 (falha).
     if [ "$permissao_certa" = true ]; then
         return 0
     else
@@ -22,12 +20,11 @@ verificar_permissoes() {
     fi
 }
 
-if [ "$#" -ne 2 ]; then #Verifica se o script recebeu exatamente dois parâmetros (diretoria de origem e diretoria de backup). Se não recebeu, exibe uma mensagem de uso correto e sai com código de erro 1
+if [ "$#" -ne 2 ]; then 
     echo "Usage: $0 <diretoria_origem> <diretoria_backup>"
     exit 1
 fi
 
-# Diretoria de origem e backup
 ORIGEM="$1"
 BACKUP="$2"
 
